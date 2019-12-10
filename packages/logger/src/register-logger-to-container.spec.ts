3907b52e9bc27641a54430064c916c607d03f3d1
Kelsey Pownell <kepownel@microsoft.com>
2019-12-09 16:58:33 -0800

    rename Logger to GlobalLogger, BaseLogger to Logger. Added new registerContextAwareLoggerToContainer function.

--- packages/logger/src/register-logger-to-container.spec.ts
+++ packages/logger/src/register-logger-to-container.spec.ts
@@ -10,17 +10,16 @@ import { AppInsightsLoggerClient } from './app-insights-logger-client';
 import { ConsoleLoggerClient } from './console-logger-client';
 import { ContextAwareAppInsightsLoggerClient } from './context-aware-app-insights-logger-client';
 import { ContextAwareConsoleLoggerClient } from './context-aware-console-logger-client';
-import { ContextAwareLogger } from './context-aware-logger';
 import { Logger } from './logger';
 import { LoggerClient } from './logger-client';
 import { loggerTypes } from './logger-types';
-import { registerLoggerToContainer } from './register-logger-to-container';
+import { registerContextAwareLoggerToContainer, registerLoggerToContainer } from './register-logger-to-container';
 
 // tslint:disable: no-unsafe-any no-any
 
-describe(registerLoggerToContainer, () => {
-    let container: Container;
+let container: Container;
 
+describe(registerLoggerToContainer, () => {
     beforeEach(() => {
         container = new Container({ autoBindInjectable: true });
     });
@@ -37,9 +36,6 @@ describe(registerLoggerToContainer, () => {
         verifySingletonDependencyResolution(AppInsightsLoggerClient);
         verifySingletonDependencyResolution(ConsoleLoggerClient);
         verifySingletonDependencyResolution(loggerTypes.DotEnvConfig);
-
-        verifyNonSingletonDependencyResolution(ContextAwareAppInsightsLoggerClient);
-        verifyNonSingletonDependencyResolution(ContextAwareConsoleLoggerClient);
     });
 
     it('verify logger resolution', () => {
@@ -53,26 +49,47 @@ describe(registerLoggerToContainer, () => {
         expect(telemetryClients.filter(c => c instanceof AppInsightsLoggerClient)).toHaveLength(1);
         expect(telemetryClients.filter(c => c instanceof ConsoleLoggerClient)).toHaveLength(1);
     });
+});
+
+describe(registerContextAwareLoggerToContainer, () => {
+    beforeEach(() => {
+        container = new Container({ autoBindInjectable: true });
+    });
+
+    it('verify logger dependency resolution', () => {
+        registerContextAwareLoggerToContainer(container);
+
+        expect(container.get(loggerTypes.AppInsights)).toBe(appInsights);
+        expect(container.get(loggerTypes.Process)).toBe(process);
+        expect(container.get(loggerTypes.Console)).toBe(console);
+        expect(container.get(loggerTypes.Argv)).toBe(argv);
+        expect(container.get(loggerTypes.DotEnvConfig)).toEqual(dotenv.config());
+
+        verifySingletonDependencyResolution(AppInsightsLoggerClient);
+        verifySingletonDependencyResolution(ConsoleLoggerClient);
+        verifySingletonDependencyResolution(loggerTypes.DotEnvConfig);
+
+        verifyNonSingletonDependencyResolution(ContextAwareAppInsightsLoggerClient);
+        verifyNonSingletonDependencyResolution(ContextAwareConsoleLoggerClient);
+    });
 
     it('verify context logger resolution', () => {
-        registerLoggerToContainer(container);
+        registerContextAwareLoggerToContainer(container);
 
-        const logger = container.get(ContextAwareLogger);
-
-        verifySingletonDependencyResolution(Logger);
+        const logger = container.get(Logger);
 
         const telemetryClients = (logger as any).loggerClients as LoggerClient[];
         expect(telemetryClients.filter(c => c instanceof ContextAwareAppInsightsLoggerClient)).toHaveLength(1);
         expect(telemetryClients.filter(c => c instanceof ContextAwareConsoleLoggerClient)).toHaveLength(1);
     });
-
-    function verifySingletonDependencyResolution(key: any): void {
-        expect(container.get(key)).toBeDefined();
-        expect(container.get(key)).toBe(container.get(key));
-    }
-
-    function verifyNonSingletonDependencyResolution(key: any): void {
-        expect(container.get(key)).toBeDefined();
-        expect(container.get(key)).not.toBe(container.get(key));
-    }
 });
+
+function verifySingletonDependencyResolution(key: any): void {
+    expect(container.get(key)).toBeDefined();
+    expect(container.get(key)).toBe(container.get(key));
+}
+
+function verifyNonSingletonDependencyResolution(key: any): void {
+    expect(container.get(key)).toBeDefined();
+    expect(container.get(key)).not.toBe(container.get(key));
+}
